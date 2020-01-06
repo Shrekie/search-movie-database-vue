a5f7ae89e6b472e27264cc05df6d91b491fc2b0a
shrekie <thomaslindauer@mail.com.com>
2020-01-06 16:10:09 +0100

    Vuetify added, header component made

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <div class="d-flex align-center">{{title}}</div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+};
+</script>
+
+<style>
+</style>
