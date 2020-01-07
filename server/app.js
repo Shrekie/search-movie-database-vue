f8de970916753d9d3828f36b4e5195d296aed58c
shrekie <thomaslindauer@mail.com.com>
2020-01-07 13:19:42 +0100

    cleaner server endpoint whitespace

--- server/app.js
+++ server/app.js
@@ -6,9 +6,10 @@ var tmdb = require("tmdbv3").init(require("./env").tmdb);
 
 app.get("/search/:searchValue", (req, routeRes) => {
   let searchValue = req.params.searchValue;
-  tmdb.search.movie(searchValue, function(err, res) {
+
+  tmdb.search.movie(searchValue, (err, res) => {
     routeRes.send(res.results);
   });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
